1877e8f93217d895c7e57f2808f616a3093a3d8b
Benjamin Jackson <benj@betterrentals.com.au>
2025-05-27 17:29:47 +1000

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -33,15 +33,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_17_133523) do
     t.index ["trip_id"], name: "index_city_trips_on_trip_id"
   end
 
-  create_table "citytrips", force: :cascade do |t|
-    t.bigint "city_id", null: false
-    t.bigint "trip_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["city_id"], name: "index_citytrips_on_city_id"
-    t.index ["trip_id"], name: "index_citytrips_on_trip_id"
-  end
-
   create_table "trips", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -71,8 +62,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_17_133523) do
 
   add_foreign_key "city_trips", "cities"
   add_foreign_key "city_trips", "trips"
-  add_foreign_key "citytrips", "cities"
-  add_foreign_key "citytrips", "trips"
   add_foreign_key "user_trips", "trips"
   add_foreign_key "user_trips", "users"
 end
